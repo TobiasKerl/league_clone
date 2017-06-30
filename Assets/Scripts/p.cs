5672dd194bfb5e60688d115c25b2f1310e8ad2e4
TobiasKerl <kerl.tobias@web.de>
2017-06-30 04:14:32 +0200

    Implement simple NavMesh Movement and Obstacle

---
+++ Assets/Scripts/p.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class p : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Ray roy = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hot;
+       
+            if (Physics.Raycast(roy, out hot))
+                agent.SetDestination(hot.point);
+        }
+
+    }
+
+}
